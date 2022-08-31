385ec8e3ff01760cb205255c0b79ac817a365ae9
Galudev <lucia.galudev@gmail.com>
2022-08-31 12:36:59 +0200

    Requerir token válido para modificar el carrito

--- routes/basket.js
+++ routes/basket.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { addItem, removeItem, updateItem } = require('../controllers/basket');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+router.use(validarJWT);
 
 router.post('/', addItem);
 router.delete('/', removeItem);
