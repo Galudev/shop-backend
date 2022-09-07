69d1286e0919ce382349e558d6e95c8a67f1ba64
Galudev <lucia.galudev@gmail.com>
2022-09-07 15:57:52 +0200

    Validación de los campos de registro y login de usuarios

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,25 @@ const express = require('express');
 const router = express.Router();
 const { newUser, loginUser, revalidarToken } = require('../controllers/auth');
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { check } = require('express-validator');
 
-router.post('/new', newUser);
-router.post('/', loginUser);
+const regexPassword = /["<>]/;
+
+router.post('/new',
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email no es válido').isEmail(),
+        check('password', 'El formato de la contraseña es erróneo').isStrongPassword().not().matches(regexPassword),
+        validarCampos
+    ],
+    newUser);
+router.post('/',
+    [
+        check('email', 'El email no es válido').isEmail(),
+        check('password', 'El formato de la contraseña es erróneo').isStrongPassword().not().matches(regexPassword),
+        validarCampos
+    ],
+    loginUser);
 router.get('/renew', validarJWT, revalidarToken)
 module.exports = router;
